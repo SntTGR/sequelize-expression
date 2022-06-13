d75e837592c7d04cd61edf65d2f74270df20c2d2
snttgr <sanobili@hotmail.com>
2022-06-13 03:30:59 -0300

    Gave up trying to make the prepublishonly work calling npm pack on the tmp folder

--- build/prepublish.js
+++ build/prepublish.js
@@ -8,18 +8,20 @@ const expectedFileOutput = `${lib.name}-${lib.version}.tgz`
 console.log(`\x1b[1m\x1b[36m------------ Asserting import of package ${lib.name} v${lib.version}. ----------\x1b[0m`);
 
 rmSync('./build/tmp', { recursive: true, force:true });
+rmSync(`${expectedFileOutput}`, { force:true });
 mkdirSync('./build/tmp');
 copyFileSync('./build/package-assertion.js', './build/tmp/index.js');
 
 console.log(`\x1b[34m== Packing ${lib.name}. ==\x1b[0m`);
 
 execSync('npm pack --pack-destination ./build/tmp/');
+execSync('npm pack');
 
 console.log(`\x1b[34m== Installing ${expectedFileOutput}. ==`);
 
 // npm install inside the tmp directory
 execSync(`npm init --yes -w ./build/tmp`)
-execSync(`cd ./build/tmp/ && npm install ${expectedFileOutput}`)
+execSync(`npm install ${expectedFileOutput} -w tmp`)
 
 console.log(`\x1b[34m== Asserting require(${lib.name}). ==`);
 
